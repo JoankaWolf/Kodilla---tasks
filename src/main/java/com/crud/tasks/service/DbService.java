209b32f48cfe9a5f033424783360a8a891eb1d20
Joanna Komoszewska <joanna.komoszewska@gmail.com>
2022-09-14 19:16:22 +0200

    19.3 Zadanie: usuwanie zadania

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,8 +1,10 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,12 +14,22 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class DbService {
 
+    @Autowired
     private final TaskRepository repository;
+
     public List<Task> getAllTasks() {
         return repository.findAll();
     }
-    public Task taskFindById(long idTask){
-        return repository.findById(idTask);
+
+    public Task getTask(final Long id) throws TaskNotFoundException {
+        return repository.findById(id).orElseThrow(TaskNotFoundException::new);
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public void deleteTask(final Long idTask) {
+        repository.deleteById(idTask);
     }
 }
-
