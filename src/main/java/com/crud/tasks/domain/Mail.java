82f0b7de8c47072a380b5f53c5e486533f54a698
Joanna Komoszewska <joanna.komoszewska@gmail.com>
2022-10-30 12:30:47 +0100

    23.3 Zadanie: dodanie maila do wiadomości (ang. CC)

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Getter
+@Builder
+public class Mail {
+        private final String mailTo;
+        private String toCc;
+        private final String subject;
+        private final String message;
+}
