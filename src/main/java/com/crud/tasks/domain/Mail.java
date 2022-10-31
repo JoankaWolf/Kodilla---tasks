614c7a17410da015a2256be940ec689e2bc6c768
Joanna Komoszewska <joanna.komoszewska@gmail.com>
2022-10-31 16:16:18 +0100

    23.4 Moduł1

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -9,6 +9,5 @@ public class Mail {
         private final String mailTo;
         private final String subject;
         private final String message;
-        private final String toCc;
-
+        private String toCc;
 }
