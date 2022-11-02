df358ca3a069f867606ec8ea72f9eddddc16b4f0
Joanna Komoszewska <joanna.komoszewska@gmail.com>
2022-11-02 18:32:26 +0100

    23.4 Zadanie: problem do rozwiązania

---
+++ src/test/java/com/crud/tasks/domain/TaskTest.java
@@ -0,0 +1,38 @@
+package com.crud.tasks.domain;
+
+import com.crud.tasks.repository.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class TaskTest {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Test
+    void testTaskSave() {
+
+        //Given
+        Task task = new Task(null, "test class task", "test task");
+
+        //When
+        taskRepository.save(task);
+
+        //Then
+        long id = task.getId();
+        List<Task> tasks  = taskRepository.findAll();
+
+        assertEquals(1, tasks.size());
+
+
+        //Cleanup
+        taskRepository.deleteById(id);
+    }
+
+}
