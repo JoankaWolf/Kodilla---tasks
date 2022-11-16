392139ccb2716b80ba2a2a1535b6bc99d66d8509
Joanna Komoszewska <joanna.komoszewska@gmail.com>
2022-11-16 15:11:50 +0100

    Module 29

--- src/test/java/com/crud/tasks/controller/OldTaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/OldTaskControllerTest.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.when;
 
 
 @SpringBootTest
-class TaskControllerTest {
+class OldTaskControllerTest {
 
     @InjectMocks
     private TaskController taskController;
