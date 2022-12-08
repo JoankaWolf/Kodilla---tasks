8ef2d8673cabeb1034e833faf2bef383fb27a124
Joanna Komoszewska <joanna.komoszewska@gmail.com>
2022-12-08 18:48:15 +0100

    32.2 Zadanie: Ustawienie pozostałych wartości

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,4 +1,5 @@
 package com.crud.tasks.service;
+import com.crud.tasks.trello.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -9,14 +10,21 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("tasks_url", "https://joankawolf.github.io/");
         context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye_message", "Best regards");
+        context.setVariable("company_details", adminConfig.getInfoAppName());
+        context.setVariable("preview_message", "New task has been created!");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
